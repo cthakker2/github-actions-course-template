5b4bd4f048c91b347840e6084dd1352065250104
cthakker2 <81589054+cthakker2@users.noreply.github.com>
2022-02-20 20:59:20 -0500

    Update game.test.js

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -2,7 +2,8 @@ const Game = require('../src/game').default
 const fs = require('fs')
 
 describe('App', () => {
-  it('Contains the compiled JavaScript', async (done) => {
+  // it('Contains the compiled JavaScript', async (done) => {
+  it('Contains the compiled JavaScript', () => {
     fs.readFile('./public/main.js', 'utf8', (err, data) => {
       expect(err).toBe(null)
       expect(data).toMatchSnapshot()
